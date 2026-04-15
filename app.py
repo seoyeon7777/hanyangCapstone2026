46d39304fb4c16817c10bb9051bfc477930bcc81
seoyeon7777 <bang41103979@gmail.com>
2026-04-15 11:57:51 +0900

    프론트엔드 수정

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "서버 정상 동작 중!"
+    return render_template('index.html')  # ← 수정
+
+
+@app.route('/result')            # ← 새 라우트 추가
+def result():
+    return render_template('result.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
