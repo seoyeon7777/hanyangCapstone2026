a21a1c92f31dcff5bf6f2ff4af102a5c2b05a13f
Yeonoh Joo <jooyeonoh3526777@gmail.com>
2026-06-07 21:03:24 +0900

    Update config.py

--- blender/config.py
+++ blender/config.py
@@ -1,6 +1,6 @@
 import os
 
-BLENDER_PATH = r"D:\Program Files\Blender Foundation\Blender 4.4\blender.exe"
+BLENDER_PATH = r"C:\Program Files\Blender Foundation\Blender 4.4\blender.exe"
 
 BASE_DIR    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SCRIPT_DIR  = os.path.join(BASE_DIR, "blender")
